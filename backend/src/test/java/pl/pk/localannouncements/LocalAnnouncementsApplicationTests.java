2e32c7dd2a482adbe66f9d495d0a9bb886b16e6d
Tomasz Umański <92022903+tomasz-umanski@users.noreply.github.com>
2024-10-15 20:47:35 +0200

    [PLO-39] feat(be): initialize Java SpringBoot project (#3)

---
+++ backend/src/test/java/pl/pk/localannouncements/LocalAnnouncementsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pk.localannouncements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalAnnouncementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
