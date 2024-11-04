902c227a04b5b29e2d9f021234796b6c4e50a95f
tomasz-umanski <tomasz.umanski@gmail.com>
2024-11-04 19:54:21 +0100

    [PLO-50] feat(be): review fix

--- backend/src/main/java/pl/pk/localannouncements/usermanagement/model/entity/Token.java
+++ backend/src/main/java/pl/pk/localannouncements/usermanagement/model/entity/Token.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class Token {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.UUID)
     public UUID id;
 
     @Column(name = "token", nullable = false, unique = true)
