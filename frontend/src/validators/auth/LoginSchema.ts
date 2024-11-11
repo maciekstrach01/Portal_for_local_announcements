421ac8c9d221c724c4d4da01fe9611a43eb3a465
MikiWojak <mikizarowa8@gmail.com>
2024-11-11 13:13:29 +0100

    Extend FE validation

--- frontend/src/validators/auth/LoginSchema.ts
+++ frontend/src/validators/auth/LoginSchema.ts
@@ -3,10 +3,12 @@ import { object, string } from 'yup';
 const LoginSchema = object({
     email: string()
         .required('This field is required')
-        .email('Wrong email format'),
+        .email('Wrong email format')
+        .max(64, 'This field must have max 64 characters'),
     password: string()
         .required('This field is required')
         .min(8, 'This field must have at least 8 characters')
+        .max(48, 'This field must have max 48 characters')
 });
 
 export default LoginSchema;
